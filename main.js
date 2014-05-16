dfb5dbccc6a72fc800093e4a3ac73d1cb06e12d7
Rory O’Kane <rory@roryokane.com>
2014-05-15 23:48:12 -0400

    Allow moving the red bike with arrow keys
    Linear movement only. Might be better as accelerating movement. No boundaries.
    
    Using this Phaser example: http://examples.phaser.io/_site/view_full.html?d=input&f=keyboard.js&t=keyboard

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 window.onload = function() {
 	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-area', { preload: preload, create: create, update: update });
 	
+	var bikeHorizSpeed = 5;
+	var bikeVertSpeed = 5;
+	var player1Bike;
+	var player2Bike;
+	
 	function preload() {
 		game.load.image('logo', 'images/phaser.png');
 		game.load.image('bike-red', 'images/motorbike-red.png');
@@ -9,11 +14,20 @@ window.onload = function() {
 	
 	function create() {
 		game.stage.backgroundColor = 0x404040;
-		var player1Bike = game.add.sprite(game.world.centerX - 200, game.world.centerY, 'bike-red');
-		var player2Bike = game.add.sprite(game.world.centerX + 100, game.world.centerY, 'bike-blue');
+		player1Bike = game.add.sprite(game.world.centerX - 200, game.world.centerY, 'bike-red');
+		player2Bike = game.add.sprite(game.world.centerX + 100, game.world.centerY, 'bike-blue');
 	}
 	
 	function update() {
-		
+		if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+			player1Bike.x -= bikeHorizSpeed;
+		} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+			player1Bike.x += bikeHorizSpeed;
+		}
+		if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+			player1Bike.y -= bikeVertSpeed;
+		} else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+			player1Bike.y += bikeVertSpeed;
+		}
 	}
 };
