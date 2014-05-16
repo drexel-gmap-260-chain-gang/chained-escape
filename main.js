3910b0aefc3fd4f92572009d59baa7018b9fe524
Rory O’Kane <rory@roryokane.com>
2014-05-15 23:11:27 -0400

    Replace example image with motorbikes
    And spruce up the game page
    
    Images are placeholders, done quickly in Photoshop with the brush tool

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 window.onload = function() {
-	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-area', { preload: preload, create: create, update: update });
 	
 	function preload() {
 		game.load.image('logo', 'images/phaser.png');
+		game.load.image('bike-red', 'images/motorbike-red.png');
+		game.load.image('bike-blue', 'images/motorbike-blue.png');
 	}
 	
 	function create() {
-		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-		logo.anchor.setTo(0.5, 0.5);
+		game.stage.backgroundColor = 0x404040;
+		var player1Bike = game.add.sprite(game.world.centerX - 200, game.world.centerY, 'bike-red');
+		var player2Bike = game.add.sprite(game.world.centerX + 100, game.world.centerY, 'bike-blue');
 	}
 	
 	function update() {
