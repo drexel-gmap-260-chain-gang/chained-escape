b9333e77058fc692067fafa8a5e6b0611d677dbe
Rory O’Kane <rory@roryokane.com>
2014-05-16 03:57:33 -0400

    Make bikes moveable; separate bike and chain collisions
    And refactored and reformatted
    
    I included the Underscore.js library (http://underscorejs.org/), mainly for its `each` method. “for each .. in” is deprecated, but “for … of …” is not widely supported yet. This seems a way around that.

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 window.onload = function() {
 	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-area', { preload: preload, create: create, update: update });
 	
-	var bikeHorizSpeed = 5;
-	var bikeVertSpeed = 5;
-	var player1Bike;
-	var player2Bike;
+	var bikeHorizSpeed = 500;
+	var bikeVertSpeed = 500;
 	
-	var player1Keymap = {
-		left: Phaser.Keyboard.LEFT,
-		right: Phaser.Keyboard.RIGHT,
-		up: Phaser.Keyboard.UP,
-		down: Phaser.Keyboard.DOWN,
-	}
-	var player2Keymap = {
-		left: Phaser.Keyboard.A,
-		right: Phaser.Keyboard.D,
-		up: Phaser.Keyboard.W,
-		down: Phaser.Keyboard.S,
+	var playerBikes = {};
+	
+	var keymaps = {
+		player1: {
+			left: Phaser.Keyboard.LEFT,
+			right: Phaser.Keyboard.RIGHT,
+			up: Phaser.Keyboard.UP,
+			down: Phaser.Keyboard.DOWN,
+		},
+		player2: {
+			left: Phaser.Keyboard.A,
+			right: Phaser.Keyboard.D,
+			up: Phaser.Keyboard.W,
+			down: Phaser.Keyboard.S,
+		}
 	}
 	
 	function preload() {
@@ -28,64 +30,82 @@ window.onload = function() {
 	
 	function create() {
 		game.stage.backgroundColor = "#404040";
-		player1Bike = game.add.sprite(game.world.centerX - 200, game.world.centerY, 'bike-red');
-		player2Bike = game.add.sprite(game.world.centerX + 100, game.world.centerY, 'bike-blue');
-		game.physics.startSystem(Phaser.Physics.P2JS);  //activate physics
-		game.physics.p2.gravity.y = 1200;  //realistic gravity
+		playerBikes.player1 = game.add.sprite(game.world.centerX + 100, game.world.centerY, 'bike-blue');
+		playerBikes.player2 = game.add.sprite(game.world.centerX - 200, game.world.centerY, 'bike-red');
+		
+		game.physics.startSystem(Phaser.Physics.P2JS);
+		game.physics.p2.gravity.y = 120;
+		
+		var bikeCollisionGroup = game.physics.p2.createCollisionGroup();
+		
+		_.each(playerBikes, function(bike) {
+			game.physics.p2.enable(bike, false);
+			// hack to counteract weight of chain:
+			bike.body.data.gravityScale = -0.15;
+			
+			bike.body.setCollisionGroup(bikeCollisionGroup);
+			bike.body.collides([bikeCollisionGroup]);
+			bike.body.data.mass = 100;
+			bike.body.fixedRotation = true;
+		})
+		
+		_.each(keymaps, function(keymap) {
+			_.each(keymap, function(keyCode, direction) {
+				game.input.keyboard.addKey(keyCode);
+			});
+		});
 	
-		createRope(15,player1Bike,player2Bike);  // length, xAnchor, yAnchor
+		createChain(15, playerBikes.player1, playerBikes.player2);
 	}
 	
-	function createRope(length, startSprite,endSprite){
+	function createChain(length, startSprite, endSprite){
 		var lastRect; // if we created our first rect this will contain it
 		var xLimit = Math.abs(startSprite.x - endSprite.x);
 		var xInterval = xLimit / length;
 		var yLimit = Math.abs(startSprite.y - endSprite.y);
 		var xAnchor = startSprite.x;
 		var yAnchor = startSprite.y;
-		game.physics.p2.enable(startSprite, false);
-		startSprite.body.data.gravityScale = 0;
-		startSprite.body.data.mass = 9999;
-		startSprite.body.static = true;
-		game.physics.p2.enable(endSprite, false);
-		endSprite.body.data.gravityScale = 0;
-		endSprite.body.data.mass = 9999;
-		endSprite.body.static = true;
+		
+		var chainLinkCollisionGroup = game.physics.p2.createCollisionGroup();
+		
 		var height = 20; // height for the physics body - your image height is 8px
 		var width = 16; // this is the width for the physics body… if too small the rectangles will get scrambled together
 		var maxForce = 20000; // the force that holds the rectangles together
 		for (var i=0; i<=length; i++) {
 			var x = xAnchor+(i*xInterval); // all rects are on the same x position
 			var y = yAnchor; // every new rects is positioned below the last
-			if (i%2==0) {
+			if (i%2 == 0) {
 				newRect = game.add.sprite(x, y, 'chain-link-1'); // add sprite
 			} else {
-				newRect = game.add.sprite(x, y, 'chain-link-2'); lastRect.bringToTop();
+				newRect = game.add.sprite(x, y, 'chain-link-2');
+				lastRect.bringToTop();
 			} // optical polish ..
 			game.physics.p2.enable(newRect, false); // enable physicsbody
 			newRect.body.setRectangle(width, height); // set custom rectangle
+			newRect.body.setCollisionGroup(chainLinkCollisionGroup);
+			newRect.body.collides([chainLinkCollisionGroup]);
 			
 			if (i == 0) {
 				game.physics.p2.createRevoluteConstraint(newRect, [0,-10], startSprite, [0,10], maxForce);
 				// anchor the first one created
 			} else {
-				newRect.body.velocity.x =100; //give it a push :)
-				newRect.body.mass = 2; // reduce mass for evey rope element
+				newRect.body.velocity.x = 100; // give it a push :)
+				newRect.body.mass = 2; // reduce mass for every rope element
 			}
 			// after the first rectangle is created we can add the constraint
 			if (lastRect) {
 				game.physics.p2.createRevoluteConstraint(newRect, [0,-10], lastRect, [0,10], maxForce);
 			}
 			lastRect = newRect;
-			if (i==length) {
+			if (i == length) {
 				game.physics.p2.createRevoluteConstraint(newRect, [0,-10], endSprite, [0,10], maxForce);
 			}
 		}
 	}
 	
 	function update() {
-		moveBikeWithKeys(player1Bike, player1Keymap)
-		moveBikeWithKeys(player2Bike, player2Keymap)
+		moveBikeWithKeys(playerBikes.player1, keymaps.player1)
+		moveBikeWithKeys(playerBikes.player2, keymaps.player2)
 	}
 	
 	function moveBikeWithKeys(sprite, keymap) {
