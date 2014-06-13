0cba1f5d7dd13648f74f9e4e118b12e25cc96976
Rory O’Kane <rory@roryokane.com>
2014-06-13 18:35:08 -0400

    Add text telling the user how to skip the opening cinematic
    for #114

--- src/OpeningCinematic.js
+++ src/OpeningCinematic.js
@@ -2,15 +2,19 @@ ChainedEscape.OpeningCinematic = function(game) {};
 ChainedEscape.OpeningCinematic.prototype = (function() {
 	
 	var displayedImage; // holds the current image of the comic
-	var delaysBetweenImagesInSecs = [3, 3, 4, 4];
+	var displayTimeForImagesInSecs = [3, 3, 4, 4];
 	var sirenSound;
+	var skipText;
 	var timeoutId;
 	
 	function create() {
-		displayPage1();
 		// FIXME space doesn’t work when enabled
 		//addHotkey(Phaser.Keyboard.SPACE, startGame, this);
 		addHotkey(Phaser.Keyboard.ESC, startGame, this);
+		sirenSound = game.add.audio('cops-coming');
+		
+		displayPage1();
+		skipText = game.add.text(470, 785, "Press [Esc] to skip", {font: "12px Arial", fill: "#444444", align: "right"});
 	}
 	
 	function addHotkey(keyCode, handler, handlerContext) {
@@ -20,27 +24,23 @@ ChainedEscape.OpeningCinematic.prototype = (function() {
 	
 	function displayPage1() {
 		displayedImage = game.add.image(0, 0, 'opening-comic-1');
-		timeoutId = setTimeout(displayPage2, delaysBetweenImagesInSecs[0] * 1000);
+		timeoutId = setTimeout(displayPage2, displayTimeForImagesInSecs[0] * 1000);
 	}
 	
 	function displayPage2() {
-		displayedImage.destroy();
-		displayedImage = game.add.image(0, 0, 'opening-comic-2');
-		timeoutId = setTimeout(displayPage3, delaysBetweenImagesInSecs[1] * 1000);
+		displayedImage.loadTexture('opening-comic-2');
+		timeoutId = setTimeout(displayPage3, displayTimeForImagesInSecs[1] * 1000);
 	}
 	
 	function displayPage3() {
-		displayedImage.destroy();
-		displayedImage = game.add.image(0, 0, 'opening-comic-3');
-		sirenSound = game.add.audio('cops-coming');
+		displayedImage.loadTexture('opening-comic-3');
 		sirenSound.play();
-		timeoutId = setTimeout(displayPage4, delaysBetweenImagesInSecs[2] * 1000);
+		timeoutId = setTimeout(displayPage4, displayTimeForImagesInSecs[2] * 1000);
 	}
 	
 	function displayPage4() {
-		displayedImage.destroy();
-		displayedImage = game.add.image(0, 0, 'opening-comic-4');
-		timeoutId = setTimeout(startGame, delaysBetweenImagesInSecs[3] * 1000);
+		displayedImage.loadTexture('opening-comic-4');
+		timeoutId = setTimeout(startGame, displayTimeForImagesInSecs[3] * 1000);
 	}
 	
 	function startGame() {
