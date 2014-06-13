a2e4ad55fd52e2f721778637fefdea9387d9dd0d
Rory O’Kane <rory@roryokane.com>
2014-06-13 18:06:39 -0400

    Add different display times for each opening comic image

--- src/OpeningCinematic.js
+++ src/OpeningCinematic.js
@@ -2,7 +2,7 @@ ChainedEscape.OpeningCinematic = function(game) {};
 ChainedEscape.OpeningCinematic.prototype = (function() {
 	
 	var displayedImage; // holds the current image of the comic
-	var delayBetweenImagesInMs = 3*1000;
+	var delaysBetweenImagesInSecs = [3, 3, 4, 4];
 	var sirenSound;
 	var timeoutId;
 	
@@ -20,27 +20,27 @@ ChainedEscape.OpeningCinematic.prototype = (function() {
 	
 	function displayPage1() {
 		displayedImage = game.add.image(0, 0, 'opening-comic-1');
-		timeoutId = setTimeout(displayPage2, delayBetweenImagesInMs);
+		timeoutId = setTimeout(displayPage2, delaysBetweenImagesInSecs[0] * 1000);
 	}
 	
 	function displayPage2() {
 		displayedImage.destroy();
 		displayedImage = game.add.image(0, 0, 'opening-comic-2');
-		timeoutId = setTimeout(displayPage3, delayBetweenImagesInMs);
+		timeoutId = setTimeout(displayPage3, delaysBetweenImagesInSecs[1] * 1000);
 	}
 	
 	function displayPage3() {
 		displayedImage.destroy();
 		displayedImage = game.add.image(0, 0, 'opening-comic-3');
-		timeoutId = setTimeout(displayPage4, delayBetweenImagesInMs);
 		sirenSound = game.add.audio('cops-coming');
 		sirenSound.play();
+		timeoutId = setTimeout(displayPage4, delaysBetweenImagesInSecs[2] * 1000);
 	}
 	
 	function displayPage4() {
 		displayedImage.destroy();
 		displayedImage = game.add.image(0, 0, 'opening-comic-4');
-		timeoutId = setTimeout(startGame, delayBetweenImagesInMs);
+		timeoutId = setTimeout(startGame, delaysBetweenImagesInSecs[3] * 1000);
 	}
 	
 	function startGame() {
